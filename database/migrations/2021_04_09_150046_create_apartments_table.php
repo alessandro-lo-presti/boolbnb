837a04d1910a0f36405847f524496511ba4ef418
alessandro-lo-presti <mitlopresti@hotmail.it>
2021-04-09 17:44:26 +0200

    first migration

---
+++ database/migrations/2021_04_09_150046_create_apartments_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")->constrained();
+            $table->string("title", 50)->unique();
+            $table->integer("n_rooms");
+            $table->integer("n_beds");
+            $table->integer("n_bathrooms");
+            $table->integer("mqs");
+            $table->string("address", 100);
+            $table->string("city", 30);
+            $table->integer("longitude");
+            $table->integer("latitude");
+            $table->string("image", 100);
+            $table->boolean('visibility');
+            $table->integer("visualization");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
