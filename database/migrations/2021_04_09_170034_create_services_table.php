4ff9dfd43daedc14459a6f757f20e23f645fd4b5
alessandro-lo-presti <73889893+alessandro-lo-presti@users.noreply.github.com>
2021-04-09 20:03:41 +0200

    Update 2021_04_09_170034_create_services_table.php

--- database/migrations/2021_04_09_170034_create_services_table.php
+++ database/migrations/2021_04_09_170034_create_services_table.php
@@ -16,10 +16,6 @@ class CreateServicesTable extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string("name", 20);
-<<<<<<< HEAD
-            $table->timestamps();
-=======
->>>>>>> main
         });
     }
 
