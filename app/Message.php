eb2059693793868c34ddd53a635b85c760f443ec
Fede <chiellofederico@gmail.com>
2021-04-10 17:30:33 +0200

    Create Message model

---
+++ app/Message.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    protected $fillable = [
+        'email',
+        'body',
+        'date'
+    ];
+
+    public function apartment() {
+        return $this->belongsTo("App\Apartment");
+    }
+}
