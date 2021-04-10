d31f6f934d6a0137ed14977bafa21a897621f434
Shinnolo <simo.28.09o@gmai.com>
2021-04-10 16:56:25 +0200

    Migration apartments_sponsors Sponsors

---
+++ database/migrations/2021_04_10_143620_create_sponsors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSponsorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sponsors', function (Blueprint $table) {
+            $table->id();
+            $table->smallInteger('time');
+            $table->decimal('price',$precision = 3, $scale = 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sponsors');
+    }
+}
---
+++ database/migrations/2021_04_10_144544_create_apartment_sponsor_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentSponsorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartment_sponsor', function (Blueprint $table) {
+            $table->foreignId("apartment_id")->constrained();
+            $table->foreignId("sponsor_id")->constrained();
+            $table->dateTime('begin');
+            $table->dateTime('end');
+            $table->string('payment_status',100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartment_sponsor');
+    }
+}
