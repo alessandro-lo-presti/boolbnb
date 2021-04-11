517709e03a49dff4b40444c3e3f73a35c696662a
Shinnolo <simo.28.09o@gmai.com>
2021-04-11 18:26:24 +0200

    Added buttons in apartment/index

--- resources/views/user/apartment/index.blade.php
+++ resources/views/user/apartment/index.blade.php
@@ -11,7 +11,27 @@
 
 
    @endforeach
+{{-- Aggiungi appartamento --}}
+<div>
+    <a class="btn btn-primary" href="{{route('apartment.create')}}">Aggiungi</a>
+</div>
+{{-- Mostra appartamento --}}
+<div>
+    <a class="btn btn-seondary" href="{{route('apartment.show')}}">Mostra</a>
+</div>
+ {{-- Edit appartamento --}}
+<div>
+    <a class="btn btn-warning" href="{{route('apartment.edit')}}">Cambia</a>
+</div>
+{{-- Cancella appartamento --}}
+<form action="{{route('apartment.destroy')}}" method='post'>
+    @csrf
+    @method('delete')
+    <div class="form-group">
+        <button class="btn btn-danger" type="submit">Cancella</button>
+    </div>
 
+</form>
 </div>
 @include('partials.footer')
 @endsection
