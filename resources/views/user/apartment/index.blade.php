52b16b532952640417c0233a14f87906540cc132
Shinnolo <simo.28.09o@gmai.com>
2021-04-11 18:37:51 +0200

    Btn destroy

--- resources/views/user/apartment/index.blade.php
+++ resources/views/user/apartment/index.blade.php
@@ -8,30 +8,32 @@
    {{$apartment->address}}
    {{$apartment->city}}
    <h1>Cristina</h1>
+   <a class="btn btn-danger" href="{{route('apartment.destroy',$apartment)}}">Cancella</a>
 
 
    @endforeach
-{{-- Aggiungi appartamento --}}
+{{-- Aggiungi appartamento
 <div>
     <a class="btn btn-primary" href="{{route('apartment.create')}}">Aggiungi</a>
 </div>
 {{-- Mostra appartamento --}}
-<div>
-    <a class="btn btn-seondary" href="{{route('apartment.show')}}">Mostra</a>
-</div>
+{{-- <div>
+    <a class="btn btn-seondary" href="{{route('apartment.show/{}')}}">Mostra</a>
+</div> --}}
  {{-- Edit appartamento --}}
-<div>
+{{-- <div>
     <a class="btn btn-warning" href="{{route('apartment.edit')}}">Cambia</a>
-</div>
+</div> --}}
 {{-- Cancella appartamento --}}
-<form action="{{route('apartment.destroy')}}" method='post'>
+{{-- <form action="{{route('apartment.destroy')}}" method='post'>
     @csrf
     @method('delete')
     <div class="form-group">
         <button class="btn btn-danger" type="submit">Cancella</button>
     </div>
 
-</form>
+{{-- </form> --}}
+
 </div>
 @include('partials.footer')
 @endsection
