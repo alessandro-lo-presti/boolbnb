1a06bfe5b4674e471008efed968af40dd985a1a2
Shinnolo <simo.28.09o@gmai.com>
2021-04-11 18:53:07 +0200

    Buttons fix in apartment/index

--- resources/views/user/apartment/index.blade.php
+++ resources/views/user/apartment/index.blade.php
@@ -8,14 +8,31 @@
    {{$apartment->address}}
    {{$apartment->city}}
    <h1>Cristina</h1>
-   <a class="btn btn-danger" href="{{route('apartment.destroy',$apartment)}}">Cancella</a>
-
+   <form action="{{route('apartment.destroy',$apartment->id)}}" method='post'>
+    @csrf
+    @method('delete')
+    <div class="form-group">
+        <button class="btn btn-danger" type="submit">Cancella</button>
+    </div>
+</form>
+{{-- Aggiungi appartamento --}}
+<div>
+    <a class="btn btn-primary" href="{{route('apartment.create')}}">Aggiungi</a>
+</div>
+{{-- Mostra appartamento --}}
+<div>
+    <a class="btn btn-secondary" href="{{route('apartment.show',$apartment->id)}}">Mostra</a>
+</div>
+ {{-- Edit appartamento --}}
+<div>
+    <a class="btn btn-warning" href="{{route('apartment.edit',$apartment->id)}}">Cambia</a>
+</div>
 
    @endforeach
 {{-- Aggiungi appartamento
 <div>
     <a class="btn btn-primary" href="{{route('apartment.create')}}">Aggiungi</a>
-</div>
+</div> --}}
 {{-- Mostra appartamento --}}
 {{-- <div>
     <a class="btn btn-seondary" href="{{route('apartment.show/{}')}}">Mostra</a>
