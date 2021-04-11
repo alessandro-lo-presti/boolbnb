95abc0dafe1ab4f7e439902eceb15b8af7917950
Giorgio <shadigiorgioislam@gmail.com>
2021-04-11 18:26:59 +0200

    upgrade sponsor controller

--- app/Http/Controllers/User/SponsorController.php
+++ app/Http/Controllers/User/SponsorController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers\User;
 use App\Sponsor;
+use App\Apartment;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class SponsorController extends Controller
 {
-    public function index()
-    {
+    public function index(Apartment $apartment)
+    {      
       $sponsors = Sponsor::all();
       $data = [
         'sponsors' => $sponsors
