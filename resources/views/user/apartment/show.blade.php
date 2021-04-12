4af2b6185ec4a0be02509314bb9ed407e2aadb7b
Fede <chiellofederico@gmail.com>
2021-04-12 10:44:27 +0200

    Start Apartment/Show.blade.php

--- resources/views/user/apartment/show.blade.php
+++ resources/views/user/apartment/show.blade.php
@@ -1,23 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('This is Show in User/Apartment') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>User ID</th>
+        <th>N_rooms</th>
+        <th>N_beds</th>
+        <th>N_bathrooms</th>
+        <th>Mq</th>
+        <th>Address</th>
+        <th>City</th>
+        <th>Longitude</th>
+        <th>Latitude</th>
+        <th>Image</th>
+        <th>Visibility</th>
+        <th>Visualization</th>
+        <th>Created_at</th>
+        <th>Updated_at</th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th>{{ $post->id }}</th>
+            <td>{{ $post->title }}</td>
+            <td>{{ $post->user_id }}</td>
+            <td>{{ $post->content }}</td>
+            <td><img src="{{asset('storage/'.$post->cover)}}" alt="{{ $post->title }}"></td>
+            <td>{{ $post->created_at }}</td>
+            <td>{{ $post->updated_at }}</td>
+          </tr>
+    </tbody>
+  </table>
 @endsection
