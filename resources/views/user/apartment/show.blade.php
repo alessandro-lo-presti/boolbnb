517092aa93f7e1dae306bb1d49fbf860f7a471b7
alessandro-lo-presti <mitlopresti@hotmail.it>
2021-04-12 17:21:27 +0200

    view show table look

--- resources/views/user/apartment/show.blade.php
+++ resources/views/user/apartment/show.blade.php
@@ -1,46 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<table class="table">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Title</th>
-        <th>User ID</th>
-        <th>N_rooms</th>
-        <th>N_beds</th>
-        <th>N_bathrooms</th>
-        <th>Mq</th>
-        <th>Address</th>
-        <th>City</th>
-        <th>Longitude</th>
-        <th>Latitude</th>
-        <th>Image</th>
-        <th>Visibility</th>
-        <th>Visualization</th>
-        <th>Created_at</th>
-        <th>Updated_at</th>
-      </tr>
-    </thead>
-    <tbody>
+<div class="container">
+
+  <div class="row">
+
+    <table class="table table-bordered">
+
+      <thead>
         <tr>
-            <th>{{ $apartment->id }}</th>
-            <td>{{ $apartment->title }}</td>
-            <td>{{ $apartment->user_id }}</td>
-            <td>{{ $apartment->n_rooms }}</td>
-            <td>{{ $apartment->n_beds }}</td>
-            <td>{{ $apartment->n_bathrooms }}</td>
-            <td>{{ $apartment->mqs }}</td>
-            <td>{{ $apartment->address }}</td>
-            <td>{{ $apartment->city }}</td>
-            <td>{{ $apartment->longitude }}</td>
-            <td>{{ $apartment->latitude }}</td>
-            <td><img src="{{asset('storage/'.$apartment->image)}}" alt="{{ $apartment->title }}"></td>
-            <td>{{ $apartment->visibility }}</td>
-            <td>{{ $apartment->visualization }}</td>
-            <td>{{ $apartment->created_at }}</td>
-            <td>{{ $apartment->updated_at }}</td>
-          </tr>
-    </tbody>
-  </table>
+          <th scope="col">ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">User ID</th>
+          <th scope="col">N_rooms</th>
+          <th scope="col">N_beds</th>
+          <th scope="col">N_bathrooms</th>
+          <th scope="col">Mq</th>
+          <th scope="col">Address</th>
+          <th scope="col">City</th>
+          <th scope="col">Longitude</th>
+          <th scope="col">Latitude</th>
+          <th scope="col">Image</th>
+          <th scope="col">Visibility</th>
+          {{-- <th scope="col">Visualization</th> --}}
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <th scope="row">{{ $apartment->id }}</th>
+          <td>{{ $apartment->title }}</td>
+          <td>{{ $apartment->user_id }}</td>
+          <td>{{ $apartment->n_rooms }}</td>
+          <td>{{ $apartment->n_beds }}</td>
+          <td>{{ $apartment->n_bathrooms }}</td>
+          <td>{{ $apartment->mqs }}</td>
+          <td>{{ $apartment->address }}</td>
+          <td>{{ $apartment->city }}</td>
+          <td>{{ $apartment->longitude }}</td>
+          <td>{{ $apartment->latitude }}</td>
+          <td><img src="{{asset('storage/'.$apartment->image)}}" alt="{{ $apartment->title }}"></td>
+          <td>{{ $apartment->visibility }}</td>
+          {{-- <td>{{ $apartment->visualization }}</td> --}}
+      </tbody>
+
+    </table>
+
+  </div>
+
+</div>
 @endsection
