dac18f1bba292cada6e67026777b1ae7d6ce9e31
Giorgio <shadigiorgioislam@gmail.com>
2021-04-12 12:08:16 +0200

    update user table + fix register view + fix controller user - register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,9 +50,10 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            // 'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            // 'birthday' => ['required', 'minAge:18']
         ]);
     }
 
@@ -64,10 +65,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        // dd($data['birthday'] => 'required|minAge:18');
+        // dd(date('Y-m-d') - $data['birthday']);
         return User::create([
             'name' => $data['name'],
+            'last_name' => $data['last_name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'birthday' => $data['birthday']
         ]);
     }
 }
