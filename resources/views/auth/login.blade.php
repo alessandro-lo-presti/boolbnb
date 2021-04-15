6c04f7a768df0bcd282a1d1a4798aed866441b05
Fede <chiellofederico@gmail.com>
2021-04-15 21:13:27 +0200

    modify email page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8 col-lg-6">
             <div class="card">
-                <div class="card-header text-center font-weight-bold">{{ __('Accedi') }}</div>
+                <div class="card-header text-center font-weight-bold" style="background-color: white">{{ __('Accedi') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 col-lg-6">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header text-center font-weight-bold" style="background-color: white">{{ __('Password dimenticata?') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -18,10 +18,10 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <div class="col-md-12">
+                                <div style="font-size: 15px; padding-bottom: 10px">Inserisci l'indirizzo email associato al tuo account e ti invieremo il link per resettare la tua password.</div>
+                                <input id="email" type="email" placeholder="Email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -32,9 +32,9 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                            <div class="col-md-12">
+                                <button type="submit" class="btn" style="width: 100%; background-color: rgb(34, 34, 34); color: white">
+                                    {{ __('Invia Link di ripristino') }}
                                 </button>
                             </div>
                         </div>
