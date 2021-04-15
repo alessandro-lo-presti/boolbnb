cb6a23c9d83e45fc9cecb30774cf3c77824f964c
alessandro-lo-presti <mitlopresti@hotmail.it>
2021-04-15 21:58:44 +0200

    imgage-section

--- resources/views/user/apartment/show.blade.php
+++ resources/views/user/apartment/show.blade.php
@@ -1,52 +1,84 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div id="show" class="container">
 
-  <div class="row">
+  <!--Title-->
+  <section class="row title-section">
+    <div class="col-12">
+      <h2>{{ $apartment->title }}</h2>
+    </div>
+    <div class="col-12">
+      <p>{{ $apartment->address }} {{ $apartment->city }}</p>
+    </div>
+  </section>
+  <!-- / Title-->
 
-    <table class="table table-bordered">
+  <!-- Images -->
+  <section class="row d-flex justify-content-between image-section">
 
-      <thead>
-        <tr>
-          <th scope="col">Image</th>
-          <th scope="col">ID</th>
-          <th scope="col">Title</th>
-          <th scope="col">User ID</th>
-          <th scope="col">N_rooms</th>
-          <th scope="col">N_beds</th>
-          <th scope="col">N_bathrooms</th>
-          <th scope="col">Mq</th>
-          <th scope="col">Address</th>
-          <th scope="col">City</th>
-          <th scope="col">Longitude</th>
-          <th scope="col">Latitude</th>
-          <th scope="col">Visibility</th>
-          {{-- <th scope="col">Visualization</th> --}}
-        </tr>
-      </thead>
+    <div class="image_box_left col-12 col-md-8 col-lg-6">
+      <img src="{{ $apartment->image == null ? asset('storage/covers/placeholder.png') : asset('storage/'.$apartment->image)}}">
+    </div>
 
-      <tbody>
-        <tr>
-          <td scope="row"><img src="{{ $apartment->image == null ? asset('storage/covers/placeholder.png') : asset('storage/'.$apartment->image)}}" alt="{{ $apartment->title }}"></td>
-          <th>{{ $apartment->id }}</th>
-          <td>{{ $apartment->title }}</td>
-          <td>{{ $apartment->user_id }}</td>
-          <td>{{ $apartment->n_rooms }}</td>
-          <td>{{ $apartment->n_beds }}</td>
-          <td>{{ $apartment->n_bathrooms }}</td>
-          <td>{{ $apartment->mqs }}</td>
-          <td>{{ $apartment->address }}</td>
-          <td>{{ $apartment->city }}</td>
-          <td>{{ $apartment->longitude }}</td>
-          <td>{{ $apartment->latitude }}</td>
-          <td>{{ $apartment->visibility }}</td>
-          {{-- <td>{{ $apartment->visualization }}</td> --}}
-      </tbody>
+    <div class="image_box_right d-none d-md-block col-sm-4 col-lg-6">
 
-    </table>
+      <div class="d-flex flex-row">
 
-  </div>
+        <div class="image_div">
+          <img src="{{ $apartment->image == null ? asset('storage/covers/placeholder.png') : asset('storage/'.$apartment->image)}}">
+        </div>
+
+        <div class="image_div d-none d-lg-block">
+          <img src="{{ $apartment->image == null ? asset('storage/covers/placeholder.png') : asset('storage/'.$apartment->image)}}">
+        </div>
+
+      </div>
+
+      <div class="d-flex flex-row">
+
+        <div class="image_div">
+          <img src="{{ $apartment->image == null ? asset('storage/covers/placeholder.png') : asset('storage/'.$apartment->image)}}">
+        </div>
+
+        <div class="image_div d-none d-lg-block">
+          <img src="{{ $apartment->image == null ? asset('storage/covers/placeholder.png') : asset('storage/'.$apartment->image)}}">
+        </div>
+
+      </div>
+
+    </div>
+
+  </section>
+
+  <section class"row">
+
+    <div class="col-6">
+      prova
+    </div>
+
+    <div class="col-6">
+      prova
+    </div>
+
+  </section>
 
 </div>
 @endsection
+
+
+{{--
+    <td scope="row"><img src="{{ $apartment->image == null ? asset('storage/covers/placeholder.png') : asset('storage/'.$apartment->image)}}" alt="{{ $apartment->title }}"></td>
+    <th>{{ $apartment->id }}</th>
+    <td>{{ $apartment->title }}</td>
+    <td>{{ $apartment->user_id }}</td>
+    <td>{{ $apartment->n_rooms }}</td>
+    <td>{{ $apartment->n_beds }}</td>
+    <td>{{ $apartment->n_bathrooms }}</td>
+    <td>{{ $apartment->mqs }}</td>
+    <td>{{ $apartment->address }}</td>
+    <td>{{ $apartment->city }}</td>
+    <td>{{ $apartment->longitude }}</td>
+    <td>{{ $apartment->latitude }}</td>
+    <td>{{ $apartment->visibility }}</td>
+    {{-- <td>{{ $apartment->visualization }}</td> --}}
