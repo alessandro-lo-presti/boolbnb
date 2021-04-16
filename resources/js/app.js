1d2a722fa1f406ad68c92009e6a1675a44e4a303
Emanuele <cannone.emanuele@gmail.com>
2021-04-16 14:47:10 +0200

    all section

--- resources/js/app.js
+++ resources/js/app.js
@@ -71,6 +71,23 @@ var app = new Vue({
                 cover: "https://a0.muscache.com/im/pictures/10a638e1-6aff-4313-8033-1275cec83987.jpg?im_w=320",
                 type: "animali domestici ammessi"
             }
+        ],
+        experiences: [
+            {
+                cover: 'https://a0.muscache.com/im/pictures/a6b08861-feb8-4a01-a76d-b33d2867b441.jpg?im_w=480',
+                title: 'esperienze online',
+                type: 'attività interattive dal vivo gestite dagli host'
+            },
+            {
+                cover: 'https://a0.muscache.com/im/pictures/ad109d56-2421-40cd-98e6-e114160dc85b.jpg?im_w=480',
+                title: 'esperienze',
+                type: 'Attività locali a cui partecipare ovunque ti trovi'
+            },
+            {
+                cover: 'https://a0.muscache.com/im/pictures/b9adfc39-6e2a-4e5f-b6f3-681b306fae5c.jpg?im_w=480',
+                title: 'avventure',
+                type: 'gite di più giorni con vitto e alloggio'
+            }
         ]
         
     },
