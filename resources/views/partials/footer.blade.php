aa7ef34a2166b1483865d08b0a9593c37445d024
Fede <chiellofederico@gmail.com>
2021-04-20 18:48:38 +0200

    add first row

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,57 @@
 
     <footer>
         <div class="container">
+            <div class="row d-flex align-items-center justify-content-between first">
+                <div class="box">
+                    <ul>
+                        <h6>Informazioni</h6>
+                        <li><a href="#">Come funziona Airbnb</a></li>
+                        <li><a href="#">Newsroom</a></li>
+                        <li><a href="#">Investitori</a></li>
+                        <li><a href="#">Airbnb Plus</a></li>
+                        <li><a href="#">Airbnb Luxe</a></li>
+                        <li><a href="#">HotelTonight</a></li>
+                        <li><a href="#">Airbnb for Work</a></li>
+                        <li><a href="#">Grazie agli host puoi</a></li>
+                        <li><a href="#">Opportunit&agrave; di lavoro</a></li>
+                        <li><a href="#">Lettera dei fondatori</a></li>
+                    </ul>
+                </div>
+                <div class="box">
+                    <ul>
+                        <h6>Community</h6>
+                        <li><a href="#">Diversit&agrave; e appartenenza</a></li>
+                        <li><a href="#">Accessibilit&agrave;</a></li>
+                        <li><a href="#">Airbnb Associates</a></li>
+                        <li><a href="#">Alloggi per l'emergenza</a></li>
+                        <li><a href="#">Invitare un ospite</a></li>
+                        <li><a href="#">Airbnb.org</a></li>
+                    </ul>
+                </div>
+                <div class="box">
+                    <ul>
+                        <h6>Ospita</h6>
+                        <li><a href="#">Diventa un host</a></li>
+                        <li><a href="#">Offri un'Esperienza online</a></li>
+                        <li><a href="#">Offri un'Esperienza</a></li>
+                        <li><a href="#">Ospitare responsabilmente</a></li>
+                        <li><a href="#">Centro risorse</a></li>
+                        <li><a href="#">Community Center</a></li>
+                    </ul>
+                </div>
+                <div class="box">
+                    <ul>
+                        <h6>Assistenza</h6>
+                        <li><a href="#">La nostra risposta all'emergenza COVID-19</a></li>
+                        <li><a href="#">Centro Assistenza</a></li>
+                        <li><a href="#">Opzioni di cancellazione</a></li>
+                        <li><a href="#">Servizio di supporto al vicinato</a></li>
+                        <li><a href="#">Affidabilit&agrave; e sicurezza</a></li>
+                    </ul>
+                </div>
+            </div>
             <!-- Lista verticale con link Github -->
-            <div class="row d-flex align-items-center justify-content-between">
+            <div class="row d-flex align-items-center justify-content-between second">
                 <!-- Link dettagli -->
                 <div class="box left">
                     <ul>
