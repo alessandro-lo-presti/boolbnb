fb52439522093cb8c462d3c211946fa723e0d87b
Emanuele <cannone.emanuele@gmail.com>
2021-04-26 10:34:10 +0200

    fix route

--- resources/views/user/message/index.blade.php
+++ resources/views/user/message/index.blade.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <script src="https://js.braintreegateway.com/web/dropin/1.27.0/js/dropin.min.js"></script>
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     @include('partials.header')
     <div id="message">
 
@@ -34,7 +22,7 @@
                             <td>{{ $message->date }}</td>
                             <td class="read text-center">
                                 <button class ="btn btn-success">
-                                    <a href="{{ route('message.show', $apartment->id, $message->id) }}">
+                                    <a href="{{ url('user/message/' . $apartment->id . '/' . $message->id) }}">
                                         <i class="fas fa-sign-in-alt"></i>
                                     </a>
                                 </button>
@@ -61,8 +49,8 @@
     </div>
 
     <script src="{{ asset('js/message.js') }}"></script>
-</body>
-</html>
+    
+@endsection
 
 
 
