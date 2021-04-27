f853d2c890d50de58d378a56bdb5bcc45b21b285
Shinnolo <simo.28.09o@gmai.com>
2021-04-27 16:20:36 +0200

    clean up pt3

--- resources/views/user/message/show.blade.php
+++ resources/views/user/message/show.blade.php
@@ -15,11 +15,6 @@
                 </div>
             </div>
         </div>
-
-
-
-        {{-- @include('partials.footer') --}}
-
     </div>
 
     <script src="{{ asset('js/message.js') }}"></script>
