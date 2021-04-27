04ef3bf066978a493a707f45bb0f93b8f41999fd
alessandro-lo-presti <mitlopresti@hotmail.it>
2021-04-27 12:59:41 +0200

    fix home route and margin for login, register, and recover-mail

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,9 +6,9 @@
     <div class="container">
 
       <a class="navbar-brand" href="{{ route('index') }}">
-          <img class="main"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1200px-Airbnb_Logo_B%C3%A9lo.svg.png"
-              alt="logo">
+        <img class="main"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1200px-Airbnb_Logo_B%C3%A9lo.svg.png"
+            alt="logo">
       </a>
 
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,8 +17,8 @@
 
       <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
         <ul class="navbar-nav">
-          <li class="nav-item {{  'active'}}">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <li class="nav-item active">
+            <a class="nav-link" href="{{ route('index') }}">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="{{ route('search') }}">Ricerca</a>
