1123a5fddbe82fb71a954f4f1af65d3c26aefc2a
alessandro-lo-presti <mitlopresti@hotmail.it>
2021-04-27 14:34:35 +0200

    redirect for other message

--- app/Http/Controllers/User/MessageController.php
+++ app/Http/Controllers/User/MessageController.php
@@ -4,31 +4,41 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Apartment;
 use App\Message;
-use Carbon\Carbon;
 
 class MessageController extends Controller
 {
     public function index(Apartment $apartment){
 
-        $messages = Message::where('apartment_id', $apartment->id)->get();
-        $data = [
-            'messages' => $messages,
-            'apartment' => $apartment
-        ];
 
-        return view('user.message.index', $data);
+        if(Auth::id() == $apartment->user_id) {
+          $messages = Message::where('apartment_id', $apartment->id)->get();
+          $data = [
+              'messages' => $messages,
+              'apartment' => $apartment
+          ];
+
+          return view('user.message.index', $data);
+        }
+
+        return redirect()->route('home');
+
     }
 
 
     public function show(Apartment $apartment, Message $message){
 
-        $data = [
-            'message' => $message
-        ];
+        if(Auth::id() == $apartment->user_id) {
+          $data = [
+              'message' => $message
+          ];
 
-        return view('user.message.show', $data);
+          return view('user.message.show', $data);
+        }
+
+        return redirect()->route('home');
     }
 
     public function destroy(Apartment $apartment, Message $message){
