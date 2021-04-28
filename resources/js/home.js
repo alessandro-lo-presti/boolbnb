19cf735352945e2d736abbf43a311c94892e3428
Emanuele <cannone.emanuele@gmail.com>
2021-04-28 16:47:24 +0200

    fix search

--- resources/js/home.js
+++ resources/js/home.js
@@ -10,13 +10,17 @@ var app = new Vue({
     el: '#home',
     data: {
         nextCounter: 1,
+        searchInput: '',
         host: 'http://localhost:8000',
         // host: 'http://localhost:8080/marzo/progetto-boolbnb/public',
         baseUrl: '',
         // baseUrl: '/marzo/progetto-boolbnb/public',
         counter: 0,
         pippo: 0,
+        apartments: [],
         sponsored: [],
+        suggests: [],
+        searchInput: '',
         experiences: [
             {
                 cover: 'https://a0.muscache.com/im/pictures/a6b08861-feb8-4a01-a76d-b33d2867b441.jpg?im_w=480',
@@ -35,6 +39,28 @@ var app = new Vue({
             }
         ]
 
+    },
+    methods:{
+        search() {
+            axios
+                .get('http://127.0.0.1:8000/api/homesearch?title=' + this.searchInput)
+                .then((result) => {
+                    this.apartments = result.data.response;
+                    this.searchInput = '';
+                }
+                );
+        },
+        changeSearchInput(suggest) {
+            this.searchInput = suggest;
+            this.search();
+        },
+        autocomplete() {
+            axios
+                .get('http://127.0.0.1:8000/api/autocomplete?title=' + this.searchInput)
+                .then((result) => {
+                    this.suggests = result.data.response
+                });
+        },
     },
     
     mounted(){
